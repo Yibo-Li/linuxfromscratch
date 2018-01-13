b96703e7be3838c51a270eab4e14c105f1805741
Yibo Li <gansuliyibo@126.com>
2018-01-14 06:22:07 +0800

    add new scripts

---
+++ script/6.4.Entering-the-Chroot-Environment.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# 6.4. Entering the Chroot Environment
+chroot "$LFS" /tools/bin/env -i \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='\u:\w\$ '              \
+    PATH=/bin:/usr/bin:/sbin:/usr/sbin:/tools/bin \
+    /tools/bin/bash --login +h
